5724f053cfdb58b3a08274c49328e3848edab8ff
Stephen Gruzin <stephengrzn@gmail.com>
2022-10-19 19:44:10 -0700

    Moved the header into its own folder

--- frontend/src/lib/Dashboard.svelte
+++ frontend/src/lib/Dashboard.svelte
@@ -1,63 +1,13 @@
-<script lang="ts">
-  import { Link } from "svelte-navigator";
+<script>
+  import Header from "./Header.svelte";
 </script>
 
-<header>
-  <h1>Dev Dashboard</h1>
-  <nav>
-    <ul>
-      <li><Link to="/structure"><p>Structure</p></Link></li>
-      <li><Link to="/websites"><p>Websites</p></Link></li>
-      <li class="logout"><Link to="/logout"><p>Logout</p></Link></li>
-    </ul>
-  </nav>
-</header>
+<Header />
 <main> main </main>
-<footer> footer </footer>
 
 <style>
-  header {
+  main {
     width: 100vw;
-    height: 70px;
-    background-color: #f3f3f3;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0 10px;
-  }
-
-  header h1 {
-    font-family: "Poppins", sans-serif;
-    font-size: 2rem;
-    font-weight: bold;
-  }
-
-  header nav ul {
-    display: flex;
-    gap: 10px;
-    align-items: center;
-  }
-
-  header nav ul li {
-    display: inline;
-    font-size: 1.25rem;
-    font-family: "Roboto", sans-serif;
-    border-bottom: 1px solid black;
-  }
-
-  header nav ul li p {
-    color: black;
-  }
-
-  .logout {
-    background-color: #dfdfdf;
-    padding: 10px;
-    border-radius: 5px;
-    border-bottom: none;
-    transition: background-color 0.25s linear;
-  }
-
-  .logout:hover {
-    background-color: #cfcfcf;
+    height: calc(100vh - 70px);
   }
 </style>
